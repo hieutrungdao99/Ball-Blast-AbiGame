a0cd5219ca5327a163c9dc6dc53dd356c7922a02
hieutrungdao <daotrunghiu99@gmail.com>
2023-07-07 11:44:19 +0700

    * fix and update textvalue meteor3

--- src/script/game/MeteorObj3.js
+++ src/script/game/MeteorObj3.js
@@ -1,10 +1,10 @@
-import { Container, Sprite, Texture, Ticker } from "pixi.js";
+import { Container, Sprite, Texture, Ticker,ColorMatrixFilter,Text } from "pixi.js";
 import { Manager } from "../../system/Manager";
 import * as PIXI from 'pixi.js';
 
 export class Meteor3 extends Container {
-  speedMeteorMinX = 1;
-  speedMeteorMinY = 4;
+  speedMeteorMinX = 0.5;
+  speedMeteorMinY = 2;
   isMovingUp = true;
   maxHeight = 0;
   minHeight = Manager.height - 100;
@@ -17,14 +17,24 @@ export class Meteor3 extends Container {
     this.meteorSprite = new PIXI.Sprite(this.meteorTexture);
     this.meteorSprite.anchor.set(0.5);
     this.type = "meteorMax";
+
     this.addChild(this.meteorSprite);
     Ticker.shared.add(this.update, this);
-
+    this.valueText = new PIXI.Text(this.value.toString(), { fill: "black", fontSize: 60  });
+    this.valueText.anchor.set(0.5);
+    this.meteorSprite.addChild(this.valueText);
+    this.valueText.x = this.meteorSprite.x/2;
+    this.valueText.y = this.meteorSprite.y/2;
     this.x = 0;
     this.y = 0;
+    const meteorTintFilter = new ColorMatrixFilter();
+    meteorTintFilter.tint(0xFF0000); 
+    this.meteorSprite.filters = [meteorTintFilter];
   }
 
   update(deltaTime) {
+
+    this.valueText.text = this.value.toString();
     if (this.isMovingUp) {
       this.meteorSprite.y -= this.speedMeteorMinY
       if (this.meteorSprite.y <= Manager.height / 3) {
