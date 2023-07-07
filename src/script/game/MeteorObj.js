8ac84ff7e9da26428e3ff3721b4742c353839a47
hieutrungdao <daotrunghiu99@gmail.com>
2023-07-07 11:43:32 +0700

    *fix speed and text MeteorObj

--- src/script/game/MeteorObj.js
+++ src/script/game/MeteorObj.js
@@ -1,10 +1,10 @@
-import { Container, Sprite, Texture, Ticker } from "pixi.js";
+import { Container, Sprite, Texture, Ticker, ColorMatrixFilter, Text } from "pixi.js";
 import { Manager } from "../../system/Manager";
 import * as PIXI from 'pixi.js';
 
 export class Meteor extends Container {
-  speedMeteorMinX = 1;
-  speedMeteorMinY = 4;
+  speedMeteorMinX = 0.5;
+  speedMeteorMinY = 2;
   isMovingUp = true;
   maxHeight = 0;
   minHeight = Manager.height - 100;
@@ -18,25 +18,37 @@ export class Meteor extends Container {
     this.meteorSprite.anchor.set(0.5);
     this.type = "meteorMin";
     this.addChild(this.meteorSprite);
+    // Create and add the valueText object
+    this.valueText = new PIXI.Text(this.value.toString(), { fill: "black", fontSize: 20  });
+    this.valueText.anchor.set(0.5);
+    this.meteorSprite.addChild(this.valueText);
+    this.valueText.x = this.meteorSprite.x/2;
+    this.valueText.y = this.meteorSprite.y/2;
     Ticker.shared.add(this.update, this);
-
     this.x = 0;
     this.y = 0;
+    const meteorTintFilter = new ColorMatrixFilter();
+    meteorTintFilter.tint(0x00FF00); // Màu xanh lá
+    this.meteorSprite.filters = [meteorTintFilter];
   }
 
   update(deltaTime) {
+
+    
+    this.valueText.text = this.value.toString();
+
     if (this.isMovingUp) {
-      this.meteorSprite.y -= this.speedMeteorMinY
+      this.meteorSprite.y -= this.speedMeteorMinY;
       if (this.meteorSprite.y <= Manager.height / 3) {
         this.isMovingUp = false;
       }
     } else {
-      this.meteorSprite.y += this.speedMeteorMinY
+      this.meteorSprite.y += this.speedMeteorMinY;
       if (this.meteorSprite.y >= this.minHeight) {
         this.isMovingUp = true;
       }
     }
-    this.meteorSprite.x += this.speedMeteorMinX
+    this.meteorSprite.x += this.speedMeteorMinX;
     if (this.meteorSprite.x <= 0) {
       this.meteorSprite.x = 0;
       this.speedMeteorMinX = Math.abs(this.speedMeteorMinX);
@@ -47,5 +59,6 @@ export class Meteor extends Container {
       this.speedMeteorMinX = -Math.abs(this.speedMeteorMinX);
       this.meteorSprite.scale.x = -Math.abs(this.meteorSprite.scale.x);
     }
+  
   }
 }
